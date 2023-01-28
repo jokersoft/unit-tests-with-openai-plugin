b7d0dd64d329ea16ef3e7e14b1093f87a0213c70
GitHub Action <action@github.com>
2023-01-28 16:49:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/jokersoft/unittestswithopenaiplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jokersoft/unittestswithopenaiplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jokersoft.unittestswithopenaiplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jokersoft.unittestswithopenaiplugin.MyBundle
 
 class MyApplicationService {
 
