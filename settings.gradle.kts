5485bfc9ff8b5c2dc9920445e02293c888b6f741
Yaroslav Sklabinskyi <yaroslav.sklabinskyi@comtravo.com>
2023-01-29 14:10:03 +0100

    multiAction

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
-rootProject.name = "Unit Tests with OpenAI"
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+rootProject.name = "action_basics"
